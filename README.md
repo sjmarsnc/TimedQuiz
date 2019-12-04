4290092a9ec016fe2c9f226fba0fc577c7519bac
sjmars <sjmarsnc@gmail.com>
2019-12-04 14:12:53 -0500

    all logic related to saving high scores and keeping just the highest score per set of initials working

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# TimedQuiz
-Timed Quiz application - homework 4
+# Week 4 Homework: Code Quiz
 
-This project is to create a timed quiz.  Questions are presented to the user and they must select the correct answer.  If they select the wrong answer, they hear a buzzer and the text "Incorrect" is shown for 2 seconds.  Correct answers get a bonus of 10 points, incorrect answers cause a deduction of 10 points.   At the end of the quiz any time left is added to the score.  The highest score for any set of initials is saved in local storage, and the high scores can be displayed using the "See High Scores" link in the upper left corner of the page, or after a quiz is completed and the score registered.  High scores are sorted in descending order by score.  
+## Timed Quiz application  
+
+The challenge is to create a timed quiz.  The topic is Javascript fundamentals, and the quiz is presented as multiple-choice questions. 
+
+## Rules 
+
+Questions are presented to the user and they must select the correct answer.  If they select the wrong answer, they hear a buzzer and the text "Incorrect" is shown for 2 seconds.  Correct answers get a bonus of 10 points, incorrect answers cause a deduction of 10 seconds from the available time.   At the end of the quiz any time left is added to the score.  The highest score for any set of initials is saved in local storage, and the high scores can be displayed using the "See High Scores" link in the upper left corner of the page.  They are also displayed after a quiz is completed and the latest score registered.  High scores are sorted in descending order by score. 
+
+## Implementation 
 
 The page is implemented by creating each section as a div and hiding or displaying it depending on where the user is in the sequence.  
 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,5 @@
 // various areas to hide or display 
 var welcomeArea = document.querySelector("#welcome-area"); 
-console.log(welcomeArea); 
 var quizArea = document.querySelector("#quiz-area"); 
 var finishedArea = document.querySelector("#finished-area"); 
 var highScoresArea = document.querySelector("#high-scores-area"); 
@@ -14,37 +13,35 @@ var rowEl = document.getElementById("rightOrWrong");
 var startButton = document.getElementById("start"); 
 var finalScoreEl = document.getElementById("finalscore"); 
 var backButton = document.getElementById("goBack"); 
+var noSavedScoresEl = document.getElementById("no-saved-scores");
 var saveScoreButton = document.getElementById("saveScore"); 
 var clearScoresButton = document.getElementById("clearScores"); 
 var initialsEl = document.getElementById("initials"); 
-var quizScores = [];  // initialize quizScores here 
 
+var highScores = [];  // saved high scores   
 var currentQuestion = 0;  // the currently displayed question 
-var timeLeft = 0;  
+var timeLeft = 0; 
+var quizScore = 0;  
 
-function clearScores() {
-    console.log("clearScores function called");
-    localStorage.setItem("quizScores","");  
-    quizScores = [""];
-}
 
 function displayQuestion(index) {
     questionEl.textContent = questions[index].title; 
-
-     answersEl.innerHTML = ""; 
-
-     for (var i=0; i < questions[index].choices.length; i++) {
-         var answerButton = document.createElement("button"); 
-         answerButton.setAttribute("class","btn mt-2 text-left");
-         answerButton.setAttribute("data-id",questions[index].choices[i]);
-         var questionNum = i + 1; 
-         answerButton.textContent = questionNum + ". " + questions[index].choices[i]; 
-         answersEl.appendChild(answerButton); 
-      }
+    
+    answersEl.innerHTML = ""; 
+    
+    for (var i=0; i < questions[index].choices.length; i++) {
+        var answerButton = document.createElement("button"); 
+        answerButton.setAttribute("class","btn mt-2 text-left");
+        answerButton.setAttribute("data-id",questions[index].choices[i]);
+        var questionNum = i + 1; 
+        answerButton.textContent = questionNum + ". " + questions[index].choices[i]; 
+        answersEl.appendChild(answerButton); 
+    }
 }
 
 function startQuiz() {
     timeLeft = 75; 
+    quizScore = 0;  
     timeLeftEl.value = timeLeft; 
     welcomeArea.classList.add("hide"); 
     quizArea.classList.remove("hide"); 
@@ -55,7 +52,9 @@ function startQuiz() {
 function answer(row) {     
     if (row) {
         rowEl.textContent = "Correct!"; 
-        rowEl.style = "color: green; font-style: italic; font-size: 1.5rem;"; 
+        rowEl.style = "color: green; font-style: italic; font-size: 1.5rem;";
+        quizScore += 10;  
+        console.log("quizScore: " + quizScore); 
     }
     else {
         rowEl.textContent = "Incorrect!"; 
@@ -72,8 +71,7 @@ function checkAnswer(event) {
     event.preventDefault(); 
     if (event.target.matches("button")) {
         var userAnswer = event.target.getAttribute("data-id"); 
-        var correctAnswer = questions[currentQuestion].answer; 
-        console.log("user: " + userAnswer + " correct: " + correctAnswer); 
+        var correctAnswer = questions[currentQuestion].answer;  
         rowEl.classList.remove("hide");  
         answer(userAnswer === correctAnswer); 
         currentQuestion ++; 
@@ -87,32 +85,80 @@ function checkAnswer(event) {
 }
 
 function quizDone() {
-    console.log("quizDone called");  
     quizArea.classList.add("hide"); 
     finishedArea.classList.remove("hide"); 
-    //stop timer 
-    finalScoreEl.value = timeLeft; 
+    //stop timer
+    quizScore = quizScore + timeLeft;  // need to keep score 
+    finalScoreEl.textContent = quizScore; 
+    console.log("quizDone called");  
+}
+
+function clearScores() {
+    console.log("clearScores function called");
+    localStorage.removeItem("quizHighScores"); 
+    noSavedScoresEl.textContent = "No saved scores available";
+    var oldScore = scoreTableEl.lastElementChild; 
+    while (oldScore) {
+        scoreTableEl.removeChild(oldScore);
+        oldScore = scoreTableEl.lastElementChild; 
+    } 
+}
+
+function getSavedScores() {
+    var quizScoresSaved = localStorage.getItem("quizHighScores"); 
+    if (quizScoresSaved != null) {
+        highScores = JSON.parse(localStorage.getItem("quizHighScores")); 
+    }
+    else {
+        highScores = []; 
+    }
 }
 
 function saveScore() {
     event.preventDefault(); 
     var initials = initialsEl.value;
-    initials = initials.toUpperCase();   
-    var quizScoresSaved = localStorage.getItem("quizScores"); 
-    if (quizScoresSaved != null && quizScoresSaved != "") {
-        quizScores = JSON.parse(localStorage.getItem("quizScores")); 
-        quizScores.push({initials: initials, score: timeLeft });
+    initials = initials.toUpperCase(); 
+    console.log("initials after uppercase: " + initials) ; 
+    getSavedScores(); 
+    var newScore = { initials: initials, score: quizScore} ;
+    var matchedInitials = ""; 
+    var notAdded = true; 
+    if (highScores.length > 0 ) { 
+        console.log("newScore: " + newScore.initials + " " + newScore.score); 
+        
+        // insert line where it goes according to highest score and 
+        // remove any ones with the same initials and lower scores 
+        // list should be in order by score 
+        console.log("At beginning: " + highScores); 
+        console.log("Incoming: " + initials + " " + quizScore);
+        for (var i=0; i < highScores.length; i++) {
+            console.log("i: " + i  + " highScores.initials: " + highScores[i].initials +
+                "  highScores.score: " + highScores[i].score);
+            if (quizScore > highScores[i].score && notAdded) {
+                console.log("high score"); 
+                highScores.splice(i, 0, newScore); 
+                matchedInitials = initials; 
+                notAdded = false; 
+            }
+            else if (highScores[i].initials === matchedInitials) {
+                // already added one for this user, remove the old one 
+                highScores.splice(i,1); 
+                console.log('Removed old one for initials ' + matchedInitials); 
+            }
+            else if (i === (highScores.length-1) && notAdded) {
+                highScores.push(newScore);  
+                notAdded = false; 
+            }
+            
+            
+        }
     } 
-    //  need to check for existing score for those initials  
-    
-    // either not previously stored or cleared 
     else {
-        quizScores = [{initials: initials,
-                      score: timeLeft}]; 
+        highScores[0] = newScore;  
     }
-    // sort so highest first 
-    quizScoresAsString = JSON.stringify(quizScores); 
-    localStorage.setItem("quizScores",quizScoresAsString); 
+    // save new list of high scores, already in order 
+    highScoresAsString = JSON.stringify(highScores); 
+    localStorage.setItem("quizHighScores",highScoresAsString); 
     initialsEl.value = "";  
     finishedArea.classList.add("hide"); 
     displayHighScores();   
@@ -121,13 +167,27 @@ function saveScore() {
 function displayHighScores() {
     console.log("displayHighScores function called"); 
     highScoresArea.classList.remove("hide"); 
-    welcomeArea.classList.add("hide");  
-    // display the scores 
-    scoreTableEl.innerhtml = ""; 
-    console.log(quizScores); 
-    for (var i=0; i < quizScores.length; i++) {
+    welcomeArea.classList.add("hide"); 
+    quizArea.classList.add("hide") ;
+     
+    getSavedScores(); 
+    // scoreTableEl.innerhtml = "";  this did not clear out existing scores 
+    // clear out previous list 
+    var oldScore = scoreTableEl.lastElementChild; 
+    while (oldScore) {
+        scoreTableEl.removeChild(oldScore);
+        oldScore = scoreTableEl.lastElementChild; 
+    } 
+     
+    if (highScores.length === 0) {
+        noSavedScoresEl.textContent = "No saved scores available";
+    }
+    else {
+        noSavedScoresEl.textContent = ""; 
+    }
+    for (var i=0; i < highScores.length; i++) {
         var liEl = document.createElement("li");
-        liEl.textContent = quizScores[i].initials + " - " + quizScores[i].score; 
+        liEl.textContent = highScores[i].initials + " - " + highScores[i].score; 
         console.log(liEl.textContent); 
         scoreTableEl.appendChild(liEl);  
     }
